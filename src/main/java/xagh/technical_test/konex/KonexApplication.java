3540ec00b8273a730d68e80b0f994b2cdedb9de1
Alejandro <alejito23001@gmail.com>
2023-10-22 01:48:41 -0500

    Project creation

---
+++ src/main/java/xagh/technical_test/konex/KonexApplication.java
@@ -0,0 +1,13 @@
+package xagh.technical_test.konex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xagh/technical_test/konex/KonexApplicationTests.java
@@ -0,0 +1,13 @@
+package xagh.technical_test.konex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
